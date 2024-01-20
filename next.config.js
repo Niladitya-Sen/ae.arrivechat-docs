21d160d066162f9719ae4939042a29c9092d8964
Niladitya-Sen <niladityasen.2212@gmail.com>
2024-01-20 14:48:26 +0530

    Add .env to .gitignore and set assetPrefix in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const isProd = process.env.NODE_ENV === 'production'
+
+const nextConfig = {
+    assetPrefix: isProd ? 'https://ae.arrive.waysdatalabs.com/docs' : undefined,
+}
 
 module.exports = nextConfig
